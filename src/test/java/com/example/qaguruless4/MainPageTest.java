9d9423c2879479cde31a1dcd10cdd874a4e316ce
Калугин Александр Александрович <AKalugin2@alfabank.ru>
2023-03-02 17:06:06 +0100

    format and clean

--- src/test/java/com/example/qaguruless4/MainPageTest.java
+++ src/test/java/com/example/qaguruless4/MainPageTest.java
@@ -1,14 +1,10 @@
 package com.example.qaguruless4;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
-
 import static com.codeborne.selenide.Condition.*;
-import static org.junit.jupiter.api.Assertions.*;
-
 import static com.codeborne.selenide.Selenide.*;
 
 public class MainPageTest {
